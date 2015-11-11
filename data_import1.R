0c188a43f44eb74ac3c04062a526eed56c9db842
jehopark <jepark@hmc.edu>
2015-11-11 15:41:22 -0800

    R script
    import_data.R

--- data_import1.R
+++ data_import1.R
@@ -1,7 +1,7 @@
-###########################################
+####################################
 ## Data import
 ## Result: data_frm (a data frame object)
-###########################################
+####################################
 # Get the file names in the pattern specified
 # All the raw data files have a .txt extension.
 files <- list.files(pattern="*.txt")
@@ -11,13 +11,37 @@ raw_data_array <- list() # a place holder (an empty list obj)
 # use cbind (column bind) function to make a 
 for(file in files)
 {
+  # read the datafile line by line
+  txt <- readLines(file)
+  # find all the lines starting with a number
+  ind <- grepl("^[0-9]",txt)
+  # create a new array only with the lines starting with a number
+  data_str <- txt[ind]
+  # split each line into pieces
+  fieldList <- strsplit(data_str, split = "\t")
+  # create a matrix 
+  data_mat <- matrix(
+    unlist(fieldList), 
+    nrow=length(fieldList),
+    byrow=TRUE)
+  # name the columns
+  colnames(data_mat) <- c("wavelength","absorbance")
+  # convert a matrix (with colnames) into a frame
+  data_frm <- as.data.frame(data_mat, stringsAsFactors=FALSE)
+  # type casting to numeric values
+  data_frm$wavelength <- as.numeric(data_frm$wavelength)
+  data_frm$absorbance <- as.numeric(data_frm$absorbance)
+  
   raw_data_array <- cbind(raw_data_array, readLines(file))
 }
 
+
+####################
+### OLD and GOOD ###
+####################
 # read the datafile line by line
 # txt <- readLines("CESAM_150615_00000.txt") 
 txt <- readLines(file.choose()) # brings up a file browser
-
 # find all the lines starting with a number
 ind <- grepl("^[0-9]",txt)
 # create a new array only with the lines starting with a number
@@ -26,9 +50,9 @@ data_str <- txt[ind]
 fieldList <- strsplit(data_str, split = "\t")
 # create a matrix 
 data_mat <- matrix(
-  unlist(fieldList), 
-  nrow=length(fieldList),
-  byrow=TRUE)
+    unlist(fieldList), 
+    nrow=length(fieldList),
+    byrow=TRUE)
 # name the columns
 colnames(data_mat) <- c("wavelength","absorbance")
 # convert a matrix (with colnames) into a frame
@@ -36,7 +60,6 @@ data_frm <- as.data.frame(data_mat, stringsAsFactors=FALSE)
 # type casting to numeric values
 data_frm$wavelength <- as.numeric(data_frm$wavelength)
 data_frm$absorbance <- as.numeric(data_frm$absorbance)
-
 ########################################
 ## Date and time import
 ## result: date_time (date-time object)
@@ -44,24 +67,20 @@ data_frm$absorbance <- as.numeric(data_frm$absorbance)
 # extract the date line at line 3 and save it in the POSIXct (date/time) class format
 datetime_line <- strsplit(txt[3], split=" ")
 datetime_str <- paste( # Date format %b%d%Y 
-  datetime_line[[1]][3], # month, abbreviated %b
-  datetime_line[[1]][4], # day, [01-31] %d
-  datetime_line[[1]][7], # year, four digits %Y
-  sep="")
+            datetime_line[[1]][3], # month, abbreviated %b
+            datetime_line[[1]][4], # day, [01-31] %d
+            datetime_line[[1]][7], # year, four digits %Y
+            sep="")
 # first convert the strings into date objects.
 datetime_obj <- as.Date(datetime_str, "%b%d%Y"); 
 # now merge the date and time into a single POSIXct object
 date_time <- as.POSIXct(paste(datetime_obj, datetime_line[[1]][5]), format="%Y-%m-%d %H:%M:%S")
-#
-# date_time object is stamped with the system time zone 
-# we need to figure out a consistent way to have the time zone stamp
-#
 ########################################
 ## Adding the timestamp to corresponding obs as a new column
 ## Result: data_frm (a data frame object)
 ########################################
 data_frm$timestamp <- rep(date_time, nrow(data_frm)) # repeat date_time value
-# As a result, data_frm now has three coloumns: wavelength, absorbance, and timestamp
+# As a result, data_frm has three coloumns: wavelength, absorbance, Timestamp
 
 ########################################
 ## Plot
