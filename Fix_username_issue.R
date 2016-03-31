9faaff2e27e2c53ac179988c82b97e0dcda89aef
levykome <levy@hmc.edu>
2016-03-30 20:33:37 -0700

    test

---
+++ Fix_username_issue.R
@@ -0,0 +1,15 @@
+#Configure Git with global commands. 
+#I have found this step necessary both times I ran through this process. 
+#Open up the bash version of Git and type the following:         
+#git config -–global user.name “your GitHub account name”                             
+#git config –global user.email “GitHubEmail@something.com”
+#Open Rstudio and set the path to Git executable. 
+#Go to Tools > Options > Git/SVN 
+#It is important that you find your git.exe file (as shown above). 
+#This may be located in any number of places 
+#depending on your operating system, 
+#but the location of your GIT install is a good first place to look.
+
+#from http://www.molecularecologist.com/2013/11/using-github-with-r-and-rstudio/
+
+#Test
